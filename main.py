8de4c7dfaa4015324b8c58554f667e1423ac2bc5
Bruno Campos <brunofdcampos@hotmail.com>
2025-05-12 11:44:01 +0100

    feature: now func to datetime mcp
    Signed-off-by: Bruno Campos <brunofdcampos@hotmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from datetime-mcp-server!")
+from mcp.server.fastmcp import FastMCP
+import os
+import datetime
 
+mcp = FastMCP("Datetime")
 
-if __name__ == "__main__":
-    main()
+@mcp.tool("now")
+def now() -> str:
+    """
+    Returns the current date and time.
+    """
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
